79e430f4db06f5253b1f63d27eb2b86e4c7b15d5
onigame <onigame@e35ed76e-ea08-11dd-97d9-e7a3ff7b18fb>
2009-02-05 06:53:07 +0000

    Adding code to handle tags.

--- config_strings.js
+++ config_strings.js
@@ -1,5 +1,4 @@
 /***** CONFIGURATION for Team Left Out, MIT Mystery Hunt 2009
-*****/
 
 var team_homepage_url = "http://teamleftout.org/wiki/Puzzles2009/";
 var team_wiki_url = team_homepage_url;
@@ -8,8 +7,10 @@ var domain = "teamleftout.org";
 var hunt_id = "mh09";
 var email = "czar@ofb.net";
 
+*****/
 
 /***** CONFIGURATION for Objects in Mirror, Microsoft Puzzle Hunt 2009
+*/
 
 var team_homepage_url = "http://www.keltis.us/objinmir/";
 var team_wiki_url = "http://www.emagino.net/oim/wiki/index.php/";
@@ -18,4 +19,3 @@ var domain = "emagino.net";
 var hunt_id = "msph12";
 var email = "czar@emagino.net";
 
-*/
