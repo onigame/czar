285d6b0010a263d530c0f826abbf5e32b243f9b6
onigame <onigame@e35ed76e-ea08-11dd-97d9-e7a3ff7b18fb>
2014-10-31 07:19:05 +0000

    Making it so  that the Hunt Site and Team Site open in a new tab/window.

--- trunk/config.js
+++ trunk/config.js
@@ -17,10 +17,82 @@ var leftout13_config = {
   gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
   doc_folder_id: "0B5i1K9hv1-e6TTE2LXpwYk16QzA",
   jobs_to_display: ["Puzzle Czar", "Comm Czar"],
-  hunt_url: "http://web.mit.edu/puzzle/www/",
-  hunt_info: "user:teamleftout pass:leftierthanthou",
+  hunt_url: "http://enigmavalley.com/",
+  hunt_info: "user:leftout pass:leftierthanthou",
   team_url: "https://sites.google.com/a/teamleftout.org/mh2013/hunt",
 };
 
+// Configuration for Burninators, Stanford Hunt 2013.
+var stanford13_config = {
+  server_url: "http://czar.emagino.net:8080/",
+  hunt_id: "stanford2013",
+  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
+  doc_folder_id: "0B7WUjr1PseBmdk9ybzFEU3hLSlE",
+  jobs_to_display: [],
+  hunt_url: "https://chaosinitiative.com/puzzles",
+  hunt_info: "user:burninators password: profitsnoddedcondemned",
+  team_url: "",
+};
+
+// Configuration for Demonic Robot Tyrannosaurs, Berkeley Hunt 2013.
+var berkeley13_config = {
+  server_url: "http://czar.emagino.net:433/",
+  hunt_id: "berkeley2013",
+  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
+  doc_folder_id: "0BwXGC8wDbipUd2tyWUlvT05WMjg",
+  jobs_to_display: [],
+  hunt_url: "https://ankursresearch.com/",
+  hunt_info: "user:Demonic Robot Tyrannosaurs password: fgd",
+  team_url: "",
+};
+
+// Configuration for Team Left Out, MIT Mystery Hunt 2014.
+var leftout14_config = {
+  server_url: "http://czar.emagino.net:433/",
+  hunt_id: "mh2014",
+  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
+  doc_folder_id: "0B5i1K9hv1-e6TWtoVFJUSXZ4bzQ",
+  jobs_to_display: ["Puzzle Czar", "Comm Czar"],
+  hunt_url: "http://www.aliceshrugged.com/",
+  hunt_info: "user:teamleftout pass:leftierthanthou",
+  team_url: "https://sites.google.com/a/teamleftout.org/mh2014/hunt",
+};
+
+// Configuration for Objects in Mirror, Microsoft Puzzle Hunt 2014.
+var msph2014_config = {
+  server_url: "http://czar.emagino.net:433/",
+  hunt_id: "msph2014",
+  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
+  doc_folder_id: "0BwXGC8wDbipUbHliWTZNSFZvRkU",
+  jobs_to_display: [],
+  hunt_url: "http://puzzlehunt/15/",
+  hunt_info: "user:<unknown> pass:<unknown>",
+  team_url: "http://www.pavelspuzzles.com/",
+};
+
+// Configuration for Demonic Robot Tyrannosaurs, Berkeley Hunt 2013.
+var berkeley14_config = {
+  server_url: "http://czar.emagino.net:433/",
+  hunt_id: "berkeley2014",
+  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
+  doc_folder_id: "0BwXGC8wDbipUNDZnQkR5eDluQU0",
+  jobs_to_display: [],
+  hunt_url: "https://puzzle.calumnio.us/",
+  hunt_info: "user:Demonic Robot Tyrannosaurs password: roar",
+  team_url: "",
+};
+
+// Configuration for Burninators, SUMS 2013.
+var sums14_config = {
+  server_url: "http://czar.emagino.net:433/",
+  hunt_id: "sums2014",
+  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
+  doc_folder_id: "0BwXGC8wDbipUdHNXZHhOTGFGaEk",
+  jobs_to_display: [],
+  hunt_url: "http://www.maths.usyd.edu.au/ub/sums/puzzlehunt/2014/puzzles",
+  hunt_info: "user:Burninators password: fgd",
+  team_url: "",
+};
+
 // Modify this to use a different config.
-var config = localhost_config;
+var config = sums14_config
