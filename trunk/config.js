fa8e7903ab2157dd6217e5df8567b0d952ef6767
onigame <onigame@e35ed76e-ea08-11dd-97d9-e7a3ff7b18fb>
2014-12-07 06:20:55 +0000

    Adding information on GAPI authentication and generating keys

--- trunk/config.js
+++ trunk/config.js
@@ -22,30 +22,6 @@ var leftout13_config = {
   team_url: "https://sites.google.com/a/teamleftout.org/mh2013/hunt",
 };
 
-// Configuration for Burninators, Stanford Hunt 2013.
-var stanford13_config = {
-  server_url: "http://czar.emagino.net:8080/",
-  hunt_id: "stanford2013",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0B7WUjr1PseBmdk9ybzFEU3hLSlE",
-  jobs_to_display: [],
-  hunt_url: "https://chaosinitiative.com/puzzles",
-  hunt_info: "user:burninators password: profitsnoddedcondemned",
-  team_url: "",
-};
-
-// Configuration for Demonic Robot Tyrannosaurs, Berkeley Hunt 2013.
-var berkeley13_config = {
-  server_url: "http://czar.emagino.net:433/",
-  hunt_id: "berkeley2013",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0BwXGC8wDbipUd2tyWUlvT05WMjg",
-  jobs_to_display: [],
-  hunt_url: "https://ankursresearch.com/",
-  hunt_info: "user:Demonic Robot Tyrannosaurs password: fgd",
-  team_url: "",
-};
-
 // Configuration for Team Left Out, MIT Mystery Hunt 2014.
 var leftout14_config = {
   server_url: "http://czar.emagino.net:433/",
@@ -70,29 +46,17 @@ var msph2014_config = {
   team_url: "http://www.pavelspuzzles.com/",
 };
 
-// Configuration for Demonic Robot Tyrannosaurs, Berkeley Hunt 2013.
-var berkeley14_config = {
-  server_url: "http://czar.emagino.net:433/",
-  hunt_id: "berkeley2014",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0BwXGC8wDbipUNDZnQkR5eDluQU0",
+// Configuration for W&T, Puzzle Boat 2.
+var puzzleboat2_config = {
+  server_url: "http://wczar.emagino.net:434/",
+  hunt_id: "puzzleboat2",
+  gapi_client_id: "368612528023-smkrhidhace7o0gsvta4mjk6mpcgjflk.apps.googleusercontent.com",
+  doc_folder_id: "0BwXGC8wDbipUbWs5YXo0b1hKdm8",
   jobs_to_display: [],
-  hunt_url: "https://puzzle.calumnio.us/",
-  hunt_info: "user:Demonic Robot Tyrannosaurs password: roar",
-  team_url: "",
-};
-
-// Configuration for Burninators, SUMS 2013.
-var sums14_config = {
-  server_url: "http://czar.emagino.net:433/",
-  hunt_id: "sums2014",
-  gapi_client_id: "368612528023-n65vkgeithp9k7ch3nr2e9rubnjqj5ib.apps.googleusercontent.com",
-  doc_folder_id: "0BwXGC8wDbipUdHNXZHhOTGFGaEk",
-  jobs_to_display: [],
-  hunt_url: "http://www.maths.usyd.edu.au/ub/sums/puzzlehunt/2014/puzzles",
-  hunt_info: "user:Burninators password: fgd",
+  hunt_url: "http://www.pandamagazine.com/island2/index.php?f=PlayPB2",
+  hunt_info: "",
   team_url: "",
 };
 
 // Modify this to use a different config.
-var config = sums14_config
+var config = puzzleboat2_config
