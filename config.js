c8784a81e722cf30a4dcea9b278bd107d2d81a2b
onigame <onigame@gmail.com>
2017-01-08 13:53:47 -0800

    Updated config.js to only use one gapi key.  Also rolled in some older hunt data.

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 var localhost_config = {
   stateserver_url: "http://localhost:8888/localhost",
-  gapi_client_id: "368612528023.apps.googleusercontent.com",  // localhost:8080
+  gapi_client_id: "368612528023-s8093hetppbt833d5fr9cegmmvaon9gk.apps.googleusercontent.com",
   doc_folder_id: "0B5i1K9hv1-e6VEc0QW9LTHhWVUU",  // "Czar Test Folder"
   jobs_to_display: ["Puzzle Czar", "Comm Czar"],
   hunt_url: "http://en.wikipedia.org/wiki/Puzzlehunt",
@@ -12,7 +12,7 @@ var localhost_config = {
 
 var czartest_config = {
   stateserver_url: "http://czartest.ofb.net:8888/czartest",
-  gapi_client_id: "100113902549-umetccnfbpm0irmivj03lknl7lbq08j0.apps.googleusercontent.com",
+  gapi_client_id: "368612528023-s8093hetppbt833d5fr9cegmmvaon9gk.apps.googleusercontent.com",
   doc_folder_id: "0B5i1K9hv1-e6VEc0QW9LTHhWVUU",  // "Czar Test Folder"
   template_doc_id: "1-fo_BrxgngvleyL2I9t91q4jNvcMUAzDHx9fp0nBvIo",
   jobs_to_display: ["Puzzle Czar", "Comm Czar"],
@@ -25,7 +25,7 @@ var czartest_config = {
 // Configuration for W&T, Puzzle Boat 2.
 var puzzleboat2_config = {
   stateserver_url: "http://wczar.emagino.net:434/puzzleboat2",
-  gapi_client_id: "368612528023-smkrhidhace7o0gsvta4mjk6mpcgjflk.apps.googleusercontent.com",
+  gapi_client_id: "368612528023-s8093hetppbt833d5fr9cegmmvaon9gk.apps.googleusercontent.com",
   doc_folder_id: "0BwXGC8wDbipUbWs5YXo0b1hKdm8",
   jobs_to_display: [],
   hunt_url: "http://www.pandamagazine.com/island2/index.php?f=PlayPB2",
@@ -33,14 +33,37 @@ var puzzleboat2_config = {
   team_url: "",
 };
 
-// Configuration for Wei-Hwa's testing before the MITMH 2015.
-var wh2015test_config = {
-  stateserver_url: "http://wczar.emagino.net:434/mh2015test",
-  gapi_client_id: "368612528023-smkrhidhace7o0gsvta4mjk6mpcgjflk.apps.googleusercontent.com",
-  doc_folder_id: "0BwXGC8wDbipUbWs5YXo0b1hKdm8",
-  jobs_to_display: ["Love", "Marriage"],
-  hunt_url: "http://www.pandamagazine.com/island2/index.php?f=PlayPB2",
+// Configuration for Objects in Mirror, Microsoft Puzzle Hunt 2016.
+var msph2016_config = {
+  stateserver_url: "http://czar.emagino.net:433/msph2016",
+  gapi_client_id: "368612528023-s8093hetppbt833d5fr9cegmmvaon9gk.apps.googleusercontent.com",
+  doc_folder_id: "0BwXGC8wDbipUQjZ2c2p0aDRVZHM",
+  jobs_to_display: [],
+  hunt_url: "https://puzzlehunt.research.microsoft.com/17/Team.aspx",
   hunt_info: "",
+  team_url: "http://www.pavelspuzzles.com/",
+};
+
+// Configuration for Berkeley Mystery Hunt, 2016.
+var berk2016_config = {
+  stateserver_url: "http://emagino.net:587/berk2016",
+  gapi_client_id: "368612528023-s8093hetppbt833d5fr9cegmmvaon9gk.apps.googleusercontent.com",
+  doc_folder_id: "0BwXGC8wDbipUeWx6a2hGdndYc2c",
+  jobs_to_display: [],
+  hunt_url: "https://oskibearin.space/",
+  hunt_info: "",
+  team_url: "",
+};
+
+// Configuration for WIT, SUMS.
+var sums2016_config = {
+  stateserver_url: "http://wczar.emagino.net:434/sums2016",
+  gapi_client_id: "368612528023-s8093hetppbt833d5fr9cegmmvaon9gk.apps.googleusercontent.com",
+  doc_folder_id: "0BwXGC8wDbipUVHNab0Q4Q3ZLLW8",
+  template_doc_id: "13aL5kga4BUCSiaEyfFtwJvubpNrUlvoClyEdaiyEyUQ",
+  jobs_to_display: ["Away"],
+  hunt_url: "http://www.maths.usyd.edu.au/ub/sums/puzzlehunt/2016/solve",
+  hunt_info: "Username: House of WIT  Password: fgd",
   team_url: "",
 };
 
@@ -54,7 +77,20 @@ var leftout16_config = {
   hunt_url: "http://www.mit.edu/~puzzle/currhunt.html",
   hunt_info: "Unknown yet!",
   team_url: "https://sites.google.com/a/teamleftout.org/mh2016/hunt",
-}
+};
+
+// Configuration for Team Left Out, MIT Mystery Hunt 2017.
+var leftout17_config = {
+  stateserver_url: "http://czar.teamleftout.org:433/mh2017",
+  gapi_client_id: "368612528023-s8093hetppbt833d5fr9cegmmvaon9gk.apps.googleusercontent.com",
+  doc_folder_id: "0B5i1K9hv1-e6bmNHNE4tc1lDY1U",
+  template_doc_id: "13aL5kga4BUCSiaEyfFtwJvubpNrUlvoClyEdaiyEyUQ",
+  jobs_to_display: ["Puzzle Czar", "Comm Czar"],
+  activities_to_display: ["Away", "Sleeping", "Mothing"],
+  hunt_url: "http://www.mit.edu/~puzzle/currhunt.html",
+  hunt_info: "Unknown yet!",
+  team_url: "https://sites.google.com/a/teamleftout.org/mh2017/hunt",
+};
 
 // Modify this to use a different config.
-var config = czartest_config
+var config = leftout17_config
